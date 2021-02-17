9e056f570712677e08f948d5b429f9252a23df30
btzr-io <btzr.io@gmail.com>
2021-02-17 15:44:03 -0600

    fix format

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from modules.elastic import Elastic
 from modules.latest import update_latest_stats
 from modules.popular import update_popular_stats
 
+
 def run():
     stream_types = ["music", "podcast", "audiobook"]
     for type in stream_types:
--- src/modules/popular.py
+++ src/modules/popular.py
@@ -1,6 +1,7 @@
 from modules.elastic import Elastic
 from modules.utils import write_json_file, get_current_time
 
+
 def update_popular_stats(stream_type):
     # Connecting to an Elasticsearch instance running on 'localhost:9200'
     df_streams = Elastic().get_df(
@@ -25,16 +26,20 @@ def update_popular_stats(stream_type):
     df_streams = df_streams[df_streams.stream_type == stream_type]
     df_popular = df_streams.sort_values(by="view_count", ascending=False)
 
-    df_top_discover = df_popular.groupby(
-        ["publisher_id"], sort=False
-    ).agg(view_count =  ('view_count', 'sum'), discover_count = ('discovered_at', 'count')).reset_index()
+    df_top_discover = (
+        df_popular.groupby(["publisher_id"], sort=False)
+        .agg(
+            view_count=("view_count", "sum"), discover_count=("discovered_at", "count")
+        )
+        .reset_index()
+    )
 
-    df_top = df_top_discover.sort_values(by=["discover_count", "view_count"], ascending=False)
+    df_top = df_top_discover.sort_values(
+        by=["discover_count", "view_count"], ascending=False
+    )
     df_top_channels_discover = df_top.head(5)[["publisher_id"]]
 
-    df_top_channels = df_top_channels_discover.merge(
-        df_channels, on="publisher_id"
-    )
+    df_top_channels = df_top_channels_discover.merge(df_channels, on="publisher_id")
 
     stats = {
         "updated": get_current_time(),
