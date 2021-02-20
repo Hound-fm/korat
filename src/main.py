ef776bc44e1ce3faf98c03e43bddbde4b9b7d278
btzr-io <btzr.io@gmail.com>
2021-02-19 22:01:16 -0600

    new tags frequency task

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from modules.elastic import Elastic
+from modules.tags import update_tags_stats
 from modules.latest import update_latest_stats
 from modules.popular import update_popular_stats
 
@@ -6,5 +7,6 @@ from modules.popular import update_popular_stats
 def run():
     stream_types = ["music", "podcast", "audiobook"]
     for type in stream_types:
+        update_tags_stats(type)
         update_latest_stats(type)
         update_popular_stats(type)
---
+++ src/modules/tags.py
@@ -0,0 +1,47 @@
+from modules.elastic import Elastic
+from modules.utils import write_json_file, get_current_time
+
+
+def update_tags_stats(stream_type):
+    # Connecting to an Elasticsearch instance running on 'localhost:9200'
+    df_streams = Elastic().get_df(
+        "streams_index",
+        [
+            "stream_type",
+            "genres",
+            "tags",
+            "publisher_id",
+            "publisher_title",
+            "discovered_at",
+            "repost_count",
+            "view_count",
+        ],
+    )
+
+    # Filter by genre
+    df_streams = df_streams[df_streams.stream_type == stream_type]
+
+    # Tags
+    df_tags = df_streams.explode("tags").dropna(axis="index")
+    df_tags = (
+        df_tags.groupby(["tags"], sort=False)
+        .agg(frequency=("tags", "count"))
+        .reset_index()
+    )
+    df_tags = df_tags.sort_values(by=["frequency"], ascending=False)
+
+    # genres
+    df_genres = df_streams.explode("genres").dropna(axis="index")
+    df_genres = (
+        df_genres.groupby(["genres"], sort=False)
+        .agg(frequency=("genres", "count"))
+        .reset_index()
+    )
+    df_genres = df_genres.sort_values(by=["frequency"], ascending=False)
+
+    stats = {
+        "tags": df_tags.to_dict("records"),
+        "genres": df_tags.to_dict("records"),
+    }
+
+    write_json_file(stats, f"stats/tags_frequency_{stream_type}.json")
